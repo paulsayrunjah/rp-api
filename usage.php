c9c5bf0d66f3512f6f11f16ec0d170f96772b68f
paul sayrunjah <pausepaule@gmail.com>
2020-02-21 15:02:58 +0300

    Usage Test

--- usage.php
+++ usage.php
@@ -4,5 +4,8 @@ use Src\Model\Customer;
 
 require __DIR__ . '/vendor/autoload.php';
 
-print_r(Customer::create(["name" => "Pauuz", "phone_number" => "0752344567"]));
+print_r(Customer::create(["name" => "Pauuz",
+    "phone_number" => "0752344567",
+    "type" => "individual",
+    "institution_customer_id" => 12, "date_of_birth" => "2001-09-00"]));
 echo "\n";
